228e18defbe39e4aa1e8317c9e187843668fa65b
Gianni <giannirosato@proton.me>
2026-08-04 00:39:51 -0500

    fix: armv6 macro

--- src/arm/config.h
+++ src/arm/config.h
@@ -5,13 +5,23 @@
 #define CONFIG_THUMB 0
 #define HAVE_ARMV5TE 1
 #define HAVE_ARMV6 1
-#define HAVE_ARMV6T2 0
 #define HAVE_ARMVFP 0
 #define HAVE_VFP_ARGS 0
 #if defined(WPD_ARM_NEON_ASM)
 #define HAVE_NEON 1
 #else
 #define HAVE_NEON 0
+#endif
+/* Probed by the build: Thumb-2 on the target, and in the assembler. */
+#if defined(WPD_ARM_ARMV6T2_ASM)
+#define HAVE_ARMV6T2 1
+#else
+#define HAVE_ARMV6T2 0
+#endif
+#if defined(WPD_ARM_ARMV6T2_EXTERNAL_ASM)
+#define HAVE_ARMV6T2_EXTERNAL 1
+#else
+#define HAVE_ARMV6T2_EXTERNAL 0
 #endif
 #define EXTERN_ASM
 
