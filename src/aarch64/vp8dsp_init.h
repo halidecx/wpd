f36de720f62db3bb07af7f4747e7a1bd1f431cd7
Gianni <giannirosato@proton.me>
2026-08-08 23:51:05 -0400

    fix: wire up the NEON lf kernels
    previously unwired, which was a mistake

--- src/aarch64/vp8dsp_init.h
+++ src/aarch64/vp8dsp_init.h
@@ -22,6 +22,18 @@ void ff_vp8_h_loop_filter_simple_mb_neon(uint8_t *dst, ptrdiff_t stride,
 
 VP8_V_LOOP_FILTER_SIMPLE_MB(vp8_v_loop_filter_simple_mb_neon,
                             ff_vp8_v_loop_filter16_simple_neon)
+VP8_H_LOOP_FILTER16Y_MB(vp8_h_loop_filter16y_mb_neon,
+                        ff_vp8_h_loop_filter16_neon,
+                        ff_vp8_h_loop_filter16_inner_neon)
+VP8_H_LOOP_FILTER8UV_MB(vp8_h_loop_filter8uv_mb_neon,
+                        ff_vp8_h_loop_filter8uv_neon,
+                        ff_vp8_h_loop_filter8uv_inner_neon)
+VP8_V_LOOP_FILTER16Y_MB(vp8_v_loop_filter16y_mb_neon,
+                        ff_vp8_v_loop_filter16_neon,
+                        ff_vp8_v_loop_filter16_inner_neon)
+VP8_V_LOOP_FILTER8UV_MB(vp8_v_loop_filter8uv_mb_neon,
+                        ff_vp8_v_loop_filter8uv_neon,
+                        ff_vp8_v_loop_filter8uv_inner_neon)
 
 static wpd_always_inline void ff_vp8dsp_init_aarch64(VP8DSPContext *dsp) {
     if (!(wpd_get_cpu_flags() & WPD_ARM_CPU_FLAG_NEON))
@@ -42,6 +54,11 @@ static wpd_always_inline void ff_vp8dsp_init_aarch64(VP8DSPContext *dsp) {
     dsp->vp8_h_loop_filter16y_inner = ff_vp8_h_loop_filter16_inner_neon;
     dsp->vp8_v_loop_filter8uv_inner = ff_vp8_v_loop_filter8uv_inner_neon;
     dsp->vp8_h_loop_filter8uv_inner = ff_vp8_h_loop_filter8uv_inner_neon;
+
+    dsp->vp8_h_loop_filter16y_mb = vp8_h_loop_filter16y_mb_neon;
+    dsp->vp8_h_loop_filter8uv_mb = vp8_h_loop_filter8uv_mb_neon;
+    dsp->vp8_v_loop_filter16y_mb = vp8_v_loop_filter16y_mb_neon;
+    dsp->vp8_v_loop_filter8uv_mb = vp8_v_loop_filter8uv_mb_neon;
 
     dsp->vp8_v_loop_filter_simple    = ff_vp8_v_loop_filter16_simple_neon;
     dsp->vp8_h_loop_filter_simple    = ff_vp8_h_loop_filter16_simple_neon;
